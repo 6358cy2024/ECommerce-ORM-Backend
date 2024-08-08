be291ece5f9492100ad2d0c12e13440bef8fea9b
Chris Yanez <cy2019@hotmail.com>
2024-08-07 21:03:21 -0400

    fixing routes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,22 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true, 
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        ket: 'id'
+      }
+    }
   },
   {
     sequelize,
